850c5f15f557b0101e9a8507f75d0cf8ab29ce2e
brobro332 <332bro@naver.com>
2025-03-13 22:30:00 +0900

    프로젝트 : 프로젝트 생성 및 기본 의존성 주입

---
+++ src/main/kotlin/kr/co/__service_desk/Application.kt
@@ -0,0 +1,11 @@
+package kr.co.__service_desk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
---
+++ src/test/kotlin/kr/co/__service_desk/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.__service_desk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
