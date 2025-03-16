df2222a7eb2a5a1adad1c840d3dfcc8f6bb3467d
brobro332 <332bro@naver.com>
2025-03-17 00:27:51 +0900

    기능추가 : Inquiry CRUD API 개발

--- src/main/kotlin/kr/co/samsami_service_desk/inquiry/dto/InquiryRequestDto.kt
+++ src/main/kotlin/kr/co/samsami_service_desk/inquiry/dto/InquiryRequestDto.kt
@@ -13,7 +13,10 @@ object InquiryRequestDto {
         val content: String,
 
         @Schema(description = "작성자 이메일")
-        val writerEmail: String
+        val writerEmail: String,
+
+        @Schema(description = "작성자 전화번호")
+        val writerPhoneNumber: String? = null
     )
 
     @Schema(description = "문의 목록 조회 요청")
@@ -33,14 +36,14 @@ object InquiryRequestDto {
         @Schema(description = "아이디")
         val id: Long,
 
-        @Schema(description = "작성자 이메일")
-        val writerEmail: String,
-
         @Schema(description = "제목")
         val title: String? = null,
 
         @Schema(description = "내용")
         val content: String? = null,
+
+        @Schema(description = "작성자 이메일")
+        val writerEmail: String,
 
         @Schema(description = "작성자 전화번호")
         val writerPhoneNumber: String? = null
